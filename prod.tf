5d6b4446db272f7e12856af3c41993e133fbe584
user <user@localhost>
2022-06-12 12:49:37 -0300

    defined initial web security group

--- prod.tf
+++ prod.tf
@@ -13,3 +13,37 @@ resource "aws_s3_bucket_acl" "prod_tf_course_acl" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inboud and evenrything outbound"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [
+      "179.182.187.181/32"
+    ]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "179.182.187.181/32"
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
